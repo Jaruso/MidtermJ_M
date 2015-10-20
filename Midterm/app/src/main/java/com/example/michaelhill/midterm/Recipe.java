632eb774c9663d62a087b968f58c9319af22ad17
Joe Caruso <joseph.caruso@mymail.champlain.edu>
2015-10-20 12:44:58 -0400

    comit

--- Midterm/app/src/main/java/com/example/michaelhill/midterm/Recipe.java
+++ Midterm/app/src/main/java/com/example/michaelhill/midterm/Recipe.java
@@ -2,9 +2,14 @@ package com.example.michaelhill.midterm;
 
 public class Recipe
 {
+    String mTitle;
+    String mIngredients;
     private int mPrepTime;
     private int mCookTime;
+    int mTotaltime; // in minutes
     private String mInstructions;
+    int mServes;    // # of people
+    private String mPath;    // path to image
 
     public Recipe()
     {
