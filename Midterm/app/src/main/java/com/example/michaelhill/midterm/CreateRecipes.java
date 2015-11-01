447c7e2141a141bb81a1172668780957cd5696fe
Joe Caruso <joseph.caruso@mymail.champlain.edu>
2015-11-01 16:35:08 -0500

    hh

---
+++ Midterm/app/src/main/java/com/example/michaelhill/midterm/CreateRecipes.java
@@ -0,0 +1,24 @@
+package com.example.michaelhill.midterm;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Joe on 11/1/2015.
+ */
+public class CreateRecipes {
+
+
+    public void generateRecipes(ArrayList<Recipe> Recipes){
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
